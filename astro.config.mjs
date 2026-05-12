ff3d80bbcd3704bfa65994dc86ac14e9483627a8
Adam Record <adammrecord@gmail.com>
2026-05-12 17:03:40 -0600

    Set canonical site URL to on-stage-sg.netlify.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://onstage-stgeorge.com',
+  site: 'https://on-stage-sg.netlify.app',
   trailingSlash: 'always',
   build: {
     format: 'directory',
